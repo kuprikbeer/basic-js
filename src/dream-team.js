b68ee69c5893afcf7820c1b2b4e6ef2d7fd13fcd
Zhenya <kuprikbeer@gmail.com>
2021-10-05 01:34:18 +0300

    solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,30 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+    throw new NotImplementedError('Not implemented');
 }
+//   let arr = []
+//   if (Array.isArray(members)) {
+//     for (let i = 0; i < members.length; i++) {
+//       if (typeof members[i] === 'string') {
+//         let word = members[i].split('')
+//         for (let j = 0; j < word.length; j++) {
+//           if (word[j] ===)      }
+//       }
+//     }
+//     return arr.sort().join('').toLocaleUpperCase()
+//   }
+//   return false
+// }
--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -12,7 +12,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(num) {
+  function innerSum(n) {
+    let str = n.toString();
+    let length = n.toString().length;
+    let sum = 0;
+    let a = 0;
+    for (let i = 0; i < length; i++) {
+      a = +str[i]
+      sum = sum + a;
+    }
+    return sum;
+  }
+  let a = innerSum(num);
+  if (a > 9) {
+    let z = innerSum(a)
+    return z;
+  } else return a;
 }
